56705c1ac545d4ae3c1a37f9792ffa7e91639c29
Allan <workofcode@gmail.com>
2023-03-11 16:01:51 +0000

    -Server
    - node will now auth via PB and return list of projects, N E A T

--- Server/app.js
+++ Server/app.js
@@ -11,11 +11,55 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  console.log('User landed on /')
-  res.send('Hello World!')
+
+///////////////////////////////////////////////////
+//////// AUTH / PB Related work
+///////////////////////////////////////////////////
+
+// Authenticate as an account. This will be used to view existing projects.
+const AuthenticateNormalUser = async function(username, pw){
+  const authData = await pb.collection('users').authWithPassword(username,pw);
+  console.log('Auth Success' + pb.authStore.isValid);
+  return pb.authStore.isValid;
+}
+
+
+
+///////////////////////////////////////////////////
+//////// FUNCTIONS
+///////////////////////////////////////////////////
+
+// Fetch a listing of all records.
+async function getAllRecords(){
+  const isAuthed = await AuthenticateNormalUser(process.env.ROLE_VIEW, process.env.ROLE_VIEW_PW);
+  if(isAuthed){
+    const records = await pb.collection('projects').getFullList({
+      sort: '-created',
+    });
+    return records;
+  }else{
+    console.log('auth not valid.');
+    return;
+  }
+}
+
+
+///////////////////////////////////////////////////
+//////// ROUTING
+///////////////////////////////////////////////////
+
+// Get a list of all projects, no filters.
+app.get('/projects', async (req, res) => {
+  try {
+    const result = await getAllRecords();
+    res.json(result);
+  }
+  catch (error){
+    console.log(error);
+  }
 })
 
 app.listen(port, () => {
+  console.log('Hello friend....');
   console.log(`Example app listening on port ${port}`)
 })
