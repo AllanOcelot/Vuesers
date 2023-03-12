7b1a93b733d52a5d5ad4d59ce3c6589e86a9e2e7
Allan <workofcode@gmail.com>
2023-03-12 15:24:46 +0000

    Update project card / add lineicons
    - tired of using font awesome, so trying out lineicons as an alternative
    - just mocking out some basics of the project card

--- Client/src/components/ProjectCard.vue
+++ Client/src/components/ProjectCard.vue
@@ -5,7 +5,8 @@ interface Project {
   title: string,
   desc: string,
   color: string,
-  url: string,
+  url_website: string,
+  url_github: string,
   id: number
 }
 </script>
@@ -21,13 +22,79 @@ defineProps({
 
 <template>
   <div class="project-card">
-    <a :href="project.url" target="_blank">{{project.title}}</a>
-    <p>{{project.desc}}</p>
+    <div class="top">
+      <p class="title">{{project.title}}</p>
+      <p>{{project.desc}}</p>
+    </div>
+    <div class="mid">
+      <div class="labels">
+          <span>Has Docs</span>
+          <span>Is Active</span>
+          <span>Archived</span>
+          <span>Alpha</span>
+          <span>Released</span>
+      </div>
+    </div>
+    <div class="bottom">
+
+      <div class="links">
+        <a v-if="project.url_github" :href="project.url_github" target="_blank" title="View Repo on Github">
+          <i class="lni lni-github-original"></i>
+        </a>
+        <a v-if="project.url_website" :href="project.url_website" target="_blank" title="View Project website">
+          <i class="lni lni-website"></i>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
   .project-card {
     background: #fff;
+    color: #000;
+
+    .top {
+      padding: 10px 20px;
+      .title {
+        font-size: 28px;
+      }
+    }
+
+    .mid {
+      padding: 10px 20px 5px 20px;
+      background: #f1f1f1;
+      border-top: 1px solid rgba(0,0,0,0.25);
+      border-bottom: 1px solid rgba(0,0,0,0.25);
+      .labels {
+        width: 100%;
+        text-align: center;
+        span {
+          display: inline-block;
+          margin: 0 5px 5px 0;
+          padding: 4px 8px;
+          border-radius: 6px;
+          color: #000;
+          background-color: #fff;
+        }
+      }
+    }
+
+    .bottom {
+      padding: 10px 20px;
+      .links {
+        width: 100%;
+        a{
+          color: #000;
+          font-size: 24px;
+          margin-right: 20px;
+          opacity: 0.9;
+          transition: opacity 0.3s;
+          &:hover {
+            opacity: 1;
+          }
+        }
+      }
+    }
   }
 </style>
