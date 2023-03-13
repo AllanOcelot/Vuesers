d52ce2b3c51a0702347972eb8b80d5a5ffb8b7b8
Allan <workofcode@gmail.com>
2023-03-13 19:54:36 +0000

    - Style sidebar
    - Add basic styling to the sidebar,  to allow routing / user routing in future

--- Client/src/components/Sidebar.vue
+++ Client/src/components/Sidebar.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 
+  // Display the login modal.
+  function showLogin(){
+    alert('login modal will show');
+  }
+
 </script>
 
 <template>
@@ -7,6 +12,61 @@
     <div class="top-section">
       <RouterLink to="/" class="router-home">Vuesers</RouterLink>
       <p>Find open source VueJS projects.</p>
+    </div>
+    <div class="mid">
+      <ul class="links-menu">
+        <li class="active">
+          <a href="/">
+            <span class="icon">
+              <i class="lni lni-folder"></i>
+            </span>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <span class="icon">
+              <i class="lni lni-question-circle"></i>
+            </span>
+            About Vuesers
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <span class="icon">
+              <i class="lni lni-book"></i>
+            </span>
+            Contributing to open source
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <span class="icon">
+              <i class="lni lni-arrow-up"></i>
+            </span>
+            Submit your project
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <span class="icon">
+              <i class="lni lni-check-box"></i>
+            </span>
+            FAQ
+          </a>
+        </li>
+      </ul>
+    </div>
+
+
+
+    <div class="bottom">
+      <div class="cta-login">
+        <button @click="showLogin()">
+          Login
+        </button>
+        <span>or <a href="/">Register an account</a>.</span>
+      </div>
     </div>
 
   </div>
@@ -22,8 +82,7 @@
 
     .top-section {
       padding: 20px;
-      margin-bottom: 20px;
-      border-bottom: 1px solid #f1f1f1;
+      border-bottom: 1px solid #f1f1f183;
       text-align: center;
       .router-home {
         color: #1cf8af;
@@ -42,5 +101,89 @@
         font-size: 14px;
       }
     }
+
+
+    .mid {
+      display: flex;
+      flex-direction: column;
+      flex: 1 auto;
+      background-color: rgba(0,0,0,0.5);
+
+      .links-menu {
+        padding: 0;
+        margin: 0;
+        li {
+          cursor: pointer;
+          opacity: 0.6;
+          border-bottom: 1px solid rgb(156 156 157);
+          transition-delay: 0.2s;
+          transition: all 0.3s;
+          a {
+            display: block;
+            padding: 10px 20px;
+            color: #fff;
+            text-decoration: none;
+            transition-delay: 0.2s;
+            transition: all 0.3s;
+          }
+          .icon {
+            position: relative;
+            top: 2px;
+            margin-right: 10px;
+          }
+
+          &.active,
+          &:hover{
+            opacity: 1;
+            a {
+              color: #1cf8af;
+            }
+          }
+          &.active {
+            border-right: 5px solid #1cf8af;
+          }
+
+        }
+      }
+    }
+
+
+
+
+    .bottom {
+      border-top: 1px solid #f1f1f183;
+      padding: 20px;
+      text-align: center;
+      
+      .cta-login {
+        button {
+          width: 100%;
+          border: none;
+          font-size: 18px;
+          cursor: pointer;
+          border-radius: 4px;
+          padding: 10px 20px; 
+          background: #17b181;
+          color: #fff;
+          opacity: 0.8;
+          transition: all 0.3s;
+          &:hover{
+            border: none;
+            opacity: 1;
+          }
+        }
+        span {
+          opacity: 0.9;
+          color: #fff;
+          display: block;
+          padding: 5px 0;
+          width: 100%;
+          a {
+            color: #fff;
+          }
+        }
+      }
+    }
+
   }
 </style>
